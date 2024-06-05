c163bd4fa1f2bf7f59eb536b60f7f2276158a748
h332un <hjw2645@naver.com>
2024-06-05 23:30:31 +0900

    sign-up class functions

--- B_SignupScreen.java
+++ B_SignupScreen.java
@@ -1,12 +1,15 @@
-// B_SignupScreen.java
-
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
@@ -22,6 +25,16 @@ public class B_SignupScreen extends JDialog {
     private JPasswordField textFieldConfirmPassword;
     private JRadioButton rdbtnMale;
     private JRadioButton rdbtnFemale;
+
+    public static void main(String[] args) {
+        try {
+            B_SignupScreen dialog = new B_SignupScreen();
+            dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+            dialog.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public B_SignupScreen() {
         setTitle("To-Do-List Registration Form");
@@ -84,6 +97,66 @@ public class B_SignupScreen extends JDialog {
             textFieldConfirmPassword = new JPasswordField();
             textFieldConfirmPassword.setColumns(10);
             contentPanel.add(textFieldConfirmPassword);
+        }
+        {
+            JPanel buttonPane = new JPanel();
+            buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+            getContentPane().add(buttonPane, BorderLayout.SOUTH);
+            {
+                JButton okButton = new JButton("Register");
+                okButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        char[] password = textFieldPassword.getPassword();
+                        char[] confirmPassword = textFieldConfirmPassword.getPassword();
+                        if (java.util.Arrays.equals(password, confirmPassword)) {
+                            try (FileOutputStream file = new FileOutputStream("SignupInfo.txt");
+                                    PrintWriter buffer = new PrintWriter(file)) {
+
+                                buffer.println("Full name:" + textFieldName.getText());
+                                if (rdbtnMale.isSelected()) {
+                                    buffer.println("Gender:Male");
+                                } else if (rdbtnFemale.isSelected()) {
+                                    buffer.println("Gender:Female");
+                                } else {
+                                    buffer.println("Gender:Unknown");
+                                }
+
+                                buffer.println("UserName:" + textFieldUserName.getText());
+                                buffer.println("Password:" + new String(password));
+
+                                JOptionPane.showMessageDialog(B_SignupScreen.this, "Registration Completed! Thank you");
+                                C_LoginScreen loginWindow = new C_LoginScreen();
+                                loginWindow.setVisible(true);
+                                dispose();
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                                JOptionPane.showMessageDialog(B_SignupScreen.this,
+                                        "An error occurred during registration.");
+                            } finally {
+                                java.util.Arrays.fill(password, '0');
+                                java.util.Arrays.fill(confirmPassword, '0');
+                            }
+                        } else {
+                            JOptionPane.showMessageDialog(B_SignupScreen.this,
+                                    "Passwords do not match. Please try again.");
+                        }
+                    }
+                });
+
+                okButton.setActionCommand("OK");
+                buttonPane.add(okButton);
+                getRootPane().setDefaultButton(okButton);
+            }
+            {
+                JButton cancelButton = new JButton("Cancel");
+                cancelButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        dispose();
+                    }
+                });
+                cancelButton.setActionCommand("Cancel");
+                buttonPane.add(cancelButton);
+            }
         }
     }
 }
