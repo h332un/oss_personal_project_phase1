568d029056a639b5cbbab87545fb2af71697d52f
h332un <hjw2645@naver.com>
2024-06-05 23:25:15 +0900

    main application class

---
+++ A_MainApplication.java
@@ -0,0 +1,24 @@
+// A_MainApplication.java
+
+import java.io.File;
+import javax.swing.JDialog;
+
+public class A_MainApplication {
+    public static void main(String[] args) {
+        A_MainApplication app = new A_MainApplication();
+        if (app.doRead()) {
+            C_LoginScreen login = new C_LoginScreen();
+            login.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+            login.setVisible(true);
+        } else {
+            B_SignupScreen signupScreen = new B_SignupScreen();
+            signupScreen.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+            signupScreen.setVisible(true);
+        }
+    }
+
+    private boolean doRead() {
+        File file = new File("SignupInfo.txt");
+        return file.exists();
+    }
+}
